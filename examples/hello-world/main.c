82d69a755b04d7d7a59b81c7b9af142adb668e3b
Angelo Haller <angelo@szanni.org>
2022-12-04 00:31:04 -0500

    Add some code to `hello-world` to highlight the cb crash bug.

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -7,12 +7,28 @@ int onClosing(uiWindow *w, void *data)
 	return 1;
 }
 
+int onClosingSub(uiWindow *w, void *data)
+{
+	return 1;
+}
+
+void buttonComboboxWindowOnClicked(uiButton *b, void *data)
+{
+	uiWindow *w;
+	uiCombobox *child;
+	w = uiNewWindow("Combobox", 300, 30, 0);
+	uiWindowOnClosing(w, onClosingSub, NULL);
+	child = uiNewCombobox();
+	uiWindowSetChild(w, uiControl(child));
+	uiControlShow(uiControl(w));
+}
+
 int main(void)
 {
 	uiInitOptions o = {0};
 	const char *err;
 	uiWindow *w;
-	uiLabel *l;
+	uiButton *b;
 
 	err = uiInit(&o);
 	if (err != NULL) {
@@ -25,8 +41,13 @@ int main(void)
 	w = uiNewWindow("Hello World!", 300, 30, 0);
 	uiWindowOnClosing(w, onClosing, NULL);
 
-	l = uiNewLabel("Hello, World!");
-	uiWindowSetChild(w, uiControl(l));
+	b = uiNewButton("Combobox");
+	uiButtonOnClicked(b, buttonComboboxWindowOnClicked, NULL);
+	uiWindowSetChild(w, uiControl(b));
+
+	/* Interestingly enough this works AND prevents the crash from happening
+	 * when triggering the creating through the callback later on. */
+	//buttonComboboxWindowOnClicked(uiButton *b, void *data)
 
 	uiControlShow(uiControl(w));
 	uiMain();
